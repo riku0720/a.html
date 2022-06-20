2c8572fe1818f521fd5f362eeb9d77967f02dbc8
riku0720 <c0a201230f@edu.teu.ac.jp>
2022-06-20 12:03:09 +0900

    first commit

---
+++ xmas_deco.php
@@ -0,0 +1,12 @@
+#contents #panel-index {background-color: #f9e6db;}
+section.content-part.content-plugin:nth-child(2):before {background-image: url(/aesc/img/notice_icons.png);background-position: 0 0;}
+section.content-part.content-plugin:nth-child(3):before {background-image: url(/aesc/img/xicon.png);background-position: 0 0;}
+section.content-part.content-plugin:nth-child(6):before {background-image: url(/aesc/img/xicon.png);background-position: 0 -200px;}
+section.content-part.content-plugin:nth-child(7):before {background-image: url(/aesc/img/xicon.png);background-position: 0 -50px;}
+section.content-part.content-plugin:nth-child(8):before {background-image: url(/aesc/img/xicon.png);background-position: 0 -500px;}
+section.content-part.content-plugin:nth-child(9):before {background-image: url(/aesc/img/xicon.png);background-position: 0 -450px;}
+section.content-part.content-plugin:nth-child(10):before {background-image: url(/aesc/img/xicon.png);background-position: 0 -550px;}
+section.content-part.content-plugin:nth-child(11):before {background-image: url(/aesc/img/xicon.png);background-position: 0 -450px;}
+section.content-part.content-plugin:nth-child(12):before {background-image: url(/aesc/img/xicon.png);background-position: 0 -350px;}
+section.content-part.content-plugin:nth-child(13):before {background-image: url(/aesc/img/xicon.png);background-position: 0 -200px;}
+section.content-part.content-plugin:nth-child(14):before {background-image: url(/aesc/img/xicon.png);background-position: 0 -350px;}
